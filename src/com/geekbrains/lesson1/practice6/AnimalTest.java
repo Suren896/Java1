de0ba02ddaaca5f6965c2adc442e121251a69c26
Suren <Surenarm@icloud.com>
2022-04-02 16:42:31 +0300

    Initial commit

---
+++ src/com/geekbrains/lesson1/practice6/AnimalTest.java
@@ -0,0 +1,20 @@
+package com.geekbrains.lesson1.practice6;
+
+public class AnimalTest {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Барбос", 11, "Лабрадор");
+        Animal.a ++;
+        Cat cat = new Cat("Мурзик", 7);
+        Animal.a ++;
+
+       dog.run(1350);
+        dog.swim(4);
+       cat.run(100);
+        cat.swim(2);
+
+        System.out.println(Animal.a);
+
+    }
+}
